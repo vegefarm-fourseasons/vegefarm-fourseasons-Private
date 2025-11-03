8ef46f4ec77a76d1c5f783ab6d76f34b5817365f
vegefarm-fourseasons <fourseasons.ryu@outlook.jp>
2025-11-03 22:01:49 +0900

    Add translation exports for multiple languages
    Export translation files for ten major languages.

---
+++ src/styles/components/components/components/components/components/components/components/lib/lib/`utils/supabase/translations/index.ts
@@ -0,0 +1,17 @@
+/**
+ * 翻訳ファイルのエクスポート
+ * 
+ * このファイルから全ての翻訳ファイルをエクスポートします
+ * 在日外国人向け主要10言語対応
+ */
+
+export { default as ja } from './ja';
+export { default as en } from './en';
+export { default as zh } from './zh';
+export { default as ko } from './ko';
+export { default as vi } from './vi';
+export { default as tl } from './tl';
+export { default as pt } from './pt';
+export { default as ne } from './ne';
+export { default as id } from './id';
+export { default as th } from './th';
